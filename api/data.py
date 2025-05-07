877fcc5b7d55fdd86c2b298cc5d036e0a6aa3e7c
muffu10 <mufaddalhussain360@gmail.com>
2025-05-07 17:35:47 +0300

    Initial commit - Backend Interview Project

---
+++ api/data.py
@@ -0,0 +1,9 @@
+media_data = [
+    {"media": "file1.mp4", "like_count": "4", "comment_count": "6"},
+    {"media": "file2.mp4", "like_count": "6", "comment_count": "8"},
+    {"media": "file3.mp4", "like_count": "10", "comment_count": "1"},
+    {"media": "file4.mp4", "like_count": "41", "comment_count": "26"},
+    {"media": "file5.mp4", "like_count": "36", "comment_count": "67"},
+    {"media": "file6.mp4", "like_count": "0", "comment_count": "9"},
+    {"media": "file7.mp4", "like_count": "99", "comment_count": "110"}
+]
---
+++ api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .data import media_data                    #import the list to be computed from data.py
+
+
+def home(request):
+    return render(request, 'home.html')         #To be shown on the home page
+
+def get_sum(media):
+    return media["like_count"] + media["comment_count"]       #Return Sum
+
+@api_view(['GET'])
+def unsorted_data(request):
+    return Response(media_data)                               #To Return Unsorted(Original) Data
+
+def precompute(media_data):                                   #Precomputing values, so we dont have to call again and again
+    for media in media_data:
+        media['like_count']=int(media["like_count"])          #Converting to int for sorting
+        media['comment_count']=int(media['comment_count'])
+
+precompute(media_data)
+
+@api_view(['GET'])
+def sorted_data(request):                                     #To return sorted data
+    try:
+
+        sorted_media = sorted(media_data, key=get_sum, reverse=True)       #Used Inbuilt sorting of python (TimSort- Time Complexity: O(nlogn))
+
+        return Response(sorted_media, status=status.HTTP_200_OK)
+
+    except Exception as e:                                                 #Exception handling
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)       #Returns the status code in case of error
